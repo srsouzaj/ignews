4130d19ed83251e95797f707d3c0dfa6025053e3
Jorge de Souza J Junior <jorgejrdj1@gmail.com>
2021-12-20 22:45:24 -0300

    Botão de subscribe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss'
 
@@ -15,6 +16,8 @@ export default function Home() {
           <p> Get acess to all thte publications <br />
             <span>for $9.90/Month</span>
           </p>
+          <SubscribeButton />
+
         </section>
 
         <img src="/images/avatar.svg" alt="Girl Coding" />
