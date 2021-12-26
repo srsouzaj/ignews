b3005621a6e6828780ef555c719cf8b24db6d93e
Sr. Souza <jorgejrdj1@gmail.com>
2021-12-26 20:51:38 -0300

    BUTTON

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,7 +1,7 @@
 import styles from './styles.module.scss'
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
-import { signIn, useSession } from 'next-auth/client'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 export function SignInButton() {
 
@@ -11,10 +11,11 @@ export function SignInButton() {
         <button
          type="button"
          className={styles.signInButton}
+         onClick={() => signOut()}
          > 
         <FaGithub 
         color="#04d361"/>
-        Jorge de Souza
+        {session.user.name}
         < FiX color="#737380" className={styles.closeIcon}/>
         </button>
     ) : (
