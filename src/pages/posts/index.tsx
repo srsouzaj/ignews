8624889bf103fb440ed94402ff2ee30352db349c
Sr. Souza <jorgejrdj1@gmail.com>
2022-01-06 16:49:54 -0300

    Terminando a estilização do post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -4,6 +4,7 @@ import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
 import Prismic from '@prismicio/client';
 import { RichText } from 'prismic-dom';
+import Link from 'next/link';
 
 type Post ={
     slug: string;
@@ -28,11 +29,13 @@ export default function Posts({posts}: PostProps) {
             <main className={styles.container}>
                 <div className={styles.posts}>
                     { posts.map(post => (
-                        <a key={post.slug} href="#">
+                        <Link key={post.slug} href={`/posts/${post.slug}`}>
+                        <a>
                         <time> {post.updatedAt} </time>
                         <strong>{post.title}</strong>
                         <p>{post.excerpt}</p>
                     </a>
+                    </Link>
                     ))}
                 </div>
             </main>
